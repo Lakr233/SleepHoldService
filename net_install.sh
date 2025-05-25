e49d3b8aabbc4f3f4c1e5b9b943031f24f7c9991
Lakr <git@qaq.wiki>
2025-05-25 16:09:27 +0800

    Update net_install.sh

--- net_install.sh
+++ net_install.sh
@@ -30,6 +30,7 @@ if [[ ! -d "/usr/local/sbin" ]]; then
 fi
 sudo rm -rf /usr/local/sbin/SleepHoldService
 sudo cp -f SleepHoldService /usr/local/sbin/SleepHoldService
+sudo spctl --assess --verbose=4 /usr/local/sbin/SleepHoldService
 
 echo "[*] Installing LaunchDaemon..."
 sudo xattr -cr launched.sleepholdservice.plist
